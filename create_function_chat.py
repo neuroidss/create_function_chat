6793f68bf48c08b9b63d3204a370a4cb4bd6be1c
dmitry-sukhoruchkin <dmitry.sukhoruchkin@gmail.com>
2024-08-15 17:54:05 +0300

    Add files via upload

--- create_function_chat.py
+++ create_function_chat.py
@@ -19,6 +19,10 @@ tools_global=[
                 'type': 'string',
                 'description': 'Function description',
               },
+              'use_cache': {
+                'type': 'boolean',
+                'description': 'Function use cache',
+              },
             },
             'required': ['name', 'description'],
           },
@@ -28,6 +32,7 @@ tools_global=[
 
 created_functions_global=[]
 
+use_cache_global = False
 temperature_global = 0.0
 seed_global = 0
 
@@ -39,11 +44,21 @@ debug = True
 num_ctx_global = 8192
 
 messages_global = []
-chat = True
-#chat = False
+chat_save = True
+#chat_save = False
+chat_load = True
+#chat_load = False
 
-#chat_in_create_function = True
-chat_in_create_function = False
+#chat_save_in_create_function = True
+chat_save_in_create_function = False
+#chat_load_in_create_function = True
+chat_load_in_create_function = False
+
+create_function_cache = True
+#create_function_cache = False
+create_function_responses_cache_global=[]
+
+created_functions_global=[]
 
 
 # Simulates an API call to get flight times
@@ -53,11 +68,21 @@ chat_in_create_function = False
 def create_function(**data) -> str:
   name = data['name']
   description = data['description']
+  if 'temperature' in data:
+    temperature = data['temperature']
+  else:
+    global temperature_global
+    temperature = temperature_global
+  if 'use_cache' in data:
+    use_cache = data['use_cache']
+  else:
+    global use_cache_global
+    use_cache = use_cache_global
   global client_global
   global model_global
   global messages_global
   messages = []
-  if chat and chat_in_create_function:
+  if chat_load_in_create_function:
     messages = messages_global
   client = client_global
   model = model_global
@@ -87,24 +112,65 @@ def create_function(**data) -> str:
   # First API call: Send the query and function description to the model
   global tools_global
 #  print('tools_global: ', tools_global)
-  global temperature_global
   global seed_global
   global num_ctx_global
-  options = {'temperature': temperature_global, 'seed': seed_global, 'num_ctx': num_ctx_global}
+  global create_function_responses_cache_global
+  response = None
+  if use_cache and (temperature == 0) and (not chat_load_in_create_function):
+#    import json
+    def read_create_function_responses_cache_global_from_json():
+      try:
+        with open('./create_function_responses_cache_global.json', 'r') as f:
+          global create_function_responses_cache_global
+          create_function_responses_cache_global = json.load(f)
+          return "Success: Messages loaded successfully."
+      except FileNotFoundError:
+        return "Failure: File not found. Please check the file path and try again."
+      except json.JSONDecodeError:
+        return "Failure: Invalid JSON format in the file."
+    read_create_function_responses_cache_global_from_json()
+    for cached in create_function_responses_cache_global:
+      cached_message = cached['cached_message']
+      cached_response = cached['cached_response']
+#      print('cached_message, cached_response:', cached_message, cached_response)
+      if cached_message['content'] == messages[0]['content']:
+        response = cached_response
+        print('cache used')
+        break
+#    "import json\n\n\ndef write_created_functions_global_json():\n    try:\n        with open('created_functions_global.json', 'w') as f:\n            json.dump(created_functions_global, f, indent=4)\n        return \"JSON file created successfully.\"\n    except Exception as e:\n        return f\"Failed to create JSON file: {str(e)}\"\n",
+
+#  created_functions_global.append(created_function)
+#    return created_function
+
+  if response is None:
+    options = {'temperature': temperature, 'seed': seed_global, 'num_ctx': num_ctx_global}
 #  response = await client.chat(
-  response = client.chat(
-    model=model,
+    response = client.chat(
+      model=model,
 #    messages=messages_global,
-    messages=messages,
+      messages=messages,
 #    tools=tools_global,
-    options=options,
-  )
+      options=options,
+    )
+    create_function_responses_cache_global.append({'cached_message':messages[0], 'cached_response':response})
+  if use_cache and (temperature == 0) and (not chat_load_in_create_function):
+#    import json
+    def write_create_function_responses_cache_global_json():
+      try:
+        with open('./create_function_responses_cache_global.json', 'w') as f:
+          global create_function_responses_cache_global
+          json.dump(create_function_responses_cache_global, f)
+          return "Success: create_function_responses_cache_global saved to ./create_function_responses_cache_global.json"
+      except Exception as e:
+        return f"Failure: {str(e)}"
+    write_create_function_responses_cache_global_json()
+    print('cache updated')
 
   # Add the model's response to the conversation history
   messages.append(response['message'])
 #  messages_global.append(response['message'])
-  if chat:
-    messages_global = messages_global + messages
+  if chat_save_in_create_function:
+    messages_global = messages
   response_message = response['message']
   if debug:
     print('response_message: ', response_message)
@@ -162,7 +228,8 @@ def create_function(**data) -> str:
           elif created_function_def_started:
               break
       created_function = '\n'.join(created_function_cut)
-      
+ 
+#      eval(created_function, globals())
       exec(created_function, globals())
 #      global tools_global
       tools_global.append(
@@ -183,7 +250,7 @@ def run(model: str, message: str):
   global model_global
   global messages_global
   messages = []
-  if chat:
+  if chat_load:
     messages = messages_global
   model_global = model
 #  client = ollama.AsyncClient()
@@ -214,12 +281,22 @@ def run(model: str, message: str):
   # Add the model's response to the conversation history
 #  messages_global.append(response['message'])
   messages.append(response['message'])
+  if chat_save:
+    messages_global = messages
 
+  if debug:
+    print("response['message']:", response['message'])
   # Check if the model decided to use the provided function
   if not response['message'].get('tool_calls'):
-    print("The model didn't use the function. Its response was:")
-    print(response['message']['content'])
-    return
+#    if response['message']['content'][0:12] == '[TOOL_CALLS]':
+#      print("The model uses modified function call, fixed. Its response was:")
+#      print(response['message']['content'])
+#      response['message']['content'] = ''
+#      response['message']['tool_calls'] = {'function':json.loads(response['message']['content'][13:])}
+#    else:
+      print("The model didn't use the function. Its response was:")
+      print(response['message']['content'])
+      return
 
   # Process function calls made by the model
   if response['message'].get('tool_calls'):
@@ -244,6 +321,8 @@ def run(model: str, message: str):
 
       # Add function response to the conversation
 #      messages_global.append({'role': 'tool','content': function_response,})
+      if chat_load:
+        messages = messages_global
       messages.append({'role': 'tool','content': function_response,})
 
 #  if tool['function']['name'] == 'add_two_numbers':
@@ -254,8 +333,8 @@ def run(model: str, message: str):
 #  final_response = client.chat(model=model, messages=messages_global)
   final_response = client.chat(model=model, messages=messages)
   print(final_response['message']['content'])
-  if chat:
-    messages_global = messages_global + messages
+  if chat_save:
+    messages_global = messages
   
 #  print('running created function:')
 #  await print_hello_world()
